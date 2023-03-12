516ba1e1fbf4bcfd0d8560a8057077208ecba31a
Satyam Sagar <satyamsagar1711@gmail.com>
2023-03-13 02:14:53 +0530

    dont test me

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ set -o errexit
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
-pip install gunicorn
-sudo apt-get install gunicorn
 
 python manage.py collectstatic --no-input
 python manage.py migrate
