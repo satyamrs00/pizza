1e0e94991fc03752ce7e28482097f82976b463de
Satyam Sagar <satyamsagar1711@gmail.com>
2023-03-13 01:21:32 +0530

    install gunicorn in build script

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -o errexit
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
+pip install gunicorn
 
 python manage.py collectstatic --no-input
 python manage.py migrate
